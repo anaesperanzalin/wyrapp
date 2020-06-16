9d7f15b6db15e5a39278b6cc2b01d1f4f45cb68e
anaesperanzalin <anaesperanzalin@users.noreply.github.com>
2020-06-16 09:40:35 -0700

    first commit

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+class Home extends React.Component{
+    render(){
+        return(
+            <h1>This is The Home Page</h1>
+        )
+    }
+}
+
+export default Home;
---
+++ src/components/login.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Login extends React.Component{
+
+render(){
+    return(
+    <div>
+        <h1>This is the login component hahaa</h1>
+        <p> Click on a dog and sign in</p>
+        <image src= "/images/avatars/animals.png"/>
+        </div>
+    );
+};
+};
+
+export default Login
